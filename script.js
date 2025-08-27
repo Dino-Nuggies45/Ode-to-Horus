0a71e16237bb95027d2f72dde6f027ab3a03bfe4
Dino-Nuggies45 <danamitchell1265@gmail.com>
2025-08-26 20:33:37 -0700

    last thing I do, I'm going to sleep now

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ document.querySelector('.door-container').addEventListener('click', () => {
         window.location.href = "shrine.html"
     }, 2000);
 })
+
+function toggleMenu() {
+    document.getElementById("navMenu").classList.toggle("active");
+}
+
+const modal = document.getElementById("modal");
+const modalTitle = document.getElementById("modal-title");
+const modalText = document.getElementById("modal-text");
+
+function openModal(title, text) {
+    modalTitle.textContent = title;
+    modalText.textContent = text;
+    modal.style.display = "flex";
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+document.getElementById("eye").addEventListener("click", () => {
+    openModal(" ")
+});
+
+document.getElementById("scarab").addEventListener("click", () => {
+    openModal(" ")
+});
+
+document.getElementById("falcon").addEventListener("click", () => {
+    openModal(" ")
+});
